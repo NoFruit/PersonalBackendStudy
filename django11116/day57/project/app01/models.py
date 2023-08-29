d6220497abc2efd44d244fe318574ef66484d7ab
NoFruit <mayixiao@yahoo.com>
2023-08-30 00:55:10 +0800

    day57

---
+++ django11116/day57/project/app01/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+# class == 添加一个表
+class UserInfo(models.Model):
+    # var == 添加字段
+    id = models.AutoField(primary_key=True) # 数据库中添加自增ID主键
+    username = models.CharField(max_length=20) # == varchar(20)
+    password = models.CharField(max_length=20)
---
+++ django11116/day57/project/project/__init__.py
@@ -0,0 +1,3 @@
+import pymysql
+
+pymysql.install_as_MySQLdb() # 使用pymysql代替默认MySQLdb链接数据库
